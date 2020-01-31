c1379c18f7eec84ec3ae51d251396186a809f2d3
Bobby Hall Jr <bobbyhalljr@Bobbys-MacBook.local>
2020-01-31 14:06:25 -0500

    stuck on problem 2

--- names/names.py
+++ names/names.py
@@ -10,14 +10,21 @@ f = open('names_2.txt', 'r')
 names_2 = f.read().split("\n")  # List containing 10000 names
 f.close()
 
+# def find_dups(nums):
 duplicates = []
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+    # nums.sort()
+for i in range(len(names_1), len(names_1)):
+    if names_1 == names_2:
+        duplicates.append(names_1)
+
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
+# print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
 print (f"runtime: {end_time - start_time} seconds")
 
 # ---------- Stretch Goal -----------
